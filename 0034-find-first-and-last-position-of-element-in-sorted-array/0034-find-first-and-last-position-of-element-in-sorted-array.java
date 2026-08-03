f88576ccf7fda72721b1bb7d0daaaba02f0262a0
Jayesh <bagaljayesh90@gmail.com>
2026-08-03 17:20:07 +0530

    Time: 0 ms (100%), Space: 48.3 MB (32.48%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,17 +1,48 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = -1;
-        int last = -1;
+        int firstIdx = findFirst(nums, target);
+        int lastIdx = findLast(nums, target);
         
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                if (first == -1) {
-                    first = i; // Record the first time we see it
-                }
-                last = i; // Keep updating last every time we see it
+        return new int[]{firstIdx, lastIdx};
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int firstIdx = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                firstIdx = mid;
+                high = mid - 1; // Look for earlier occurrences on the left
             }
         }
-        
-        return new int[]{first, last};
+        return firstIdx;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int lastIdx = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                lastIdx = mid;
+                low = mid + 1; // Look for later occurrences on the right
+            }
+        }
+        return lastIdx;
     }
 }
